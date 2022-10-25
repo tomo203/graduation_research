9191c60f05a87c3c2abbda53c5f7445284f93f3d
miyajima <miyajima@example.com>
2022-10-25 16:06:23 +0900

    aaaa

--- tb6643kq_driver.py
+++ tb6643kq_driver.py
@@ -2,15 +2,20 @@ import pigpio
 
 
 class tb6643kq_driver:
-    def __init__(self, pin_1=0, pin_2=0, pi="192.168.10.21", freq=1000, range=100) -> None:
+    def __init__(self, pin_1, pin_2, pi="192.168.10.21", freq=1000, range=100) -> None:
         self.PIN_1 = pin_1
         self.PIN_2 = pin_2
-        self.FREQ = freq
 
         self.pi = pigpio.pi(pi)
 
-        self.pi.set_PWM_frequency(self.PIN_1, self.FREQ)
-        self.pi.set_PWM_frequency(self.PIN_2, self.FREQ)
+        self.pi.set_PWM_frequency(self.PIN_1, freq)
+        self.pi.set_PWM_frequency(self.PIN_2, freq)
+
+        self.pi.set_PWM_range(self.PIN_1, range)
 
         self.pi.set_mode(self.PIN_1, pigpio.OUTPUT)
         self.pi.set_mode(self.PIN_2, pigpio.OUTPUT)
+
+    def drive_motor(speed):
+        if speed >= 0:
+            pass
