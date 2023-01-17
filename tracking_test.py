53a9a64cdfa8ca3d59fa48bf7fae35117ad2b2d1
tomo203 <yff59399@icloud.com>
2023-01-17 11:30:06 +0900

    a

--- tracking_test.py
+++ tracking_test.py
@@ -4,18 +4,26 @@ import cv2
 def draw_box(img, bbox):
     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
     cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 0), 3, 1)
-    cv2.putText(img, 'Tracking', (15, 70), font, 0.5, (0, 0, 255), 2)
+    cv2.putText(img, F'Tracking {x}', (15, 70), font, 0.5, (0, 0, 255), 2)
 
 
 if __name__ == '__main__':
 
     # mjpg-streamerを動作させているPC・ポートを入力
-    URL = "http://192.168.10.21:8080/?action=stream"
+    URL = "http://192.168.137.125:8080/?action=stream"
     cap = cv2.VideoCapture(URL)
 
     # Create tracker
     tracker = cv2.TrackerKCF_create()
-    ret, img = cap.read()
+
+    while True:
+        ret, img = cap.read()
+
+        cv2.imshow("Tracking", img)
+
+        key = cv2.waitKey(1)
+        if key == 32:  # Esc入力時は終了
+            break
 
     # Create bbox
     bbox = cv2.selectROI("Tracking", img, False)
@@ -31,7 +39,8 @@ if __name__ == '__main__':
         if success:
             draw_box(img, bbox)
         else:
-            cv2.putText(img, 'Tracking Lost', (15, 70), font, 0.5, (0, 0, 255), 2)
+            cv2.putText(img, 'Tracking Lost', (15, 70),
+                        font, 0.5, (0, 0, 255), 2)
 
         cv2.imshow("Tracking", img)
 
